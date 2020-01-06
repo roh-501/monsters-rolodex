515bb5d11d67071a30a4cfe2fe438b1a3b6e398c
Rohini Kadu <rkadu501@gmail.com>
2020-01-06 22:07:39 +0530

    Updates

---
+++ precache-manifest.2de85cf60cd368d0b0b32f27beb24da2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77469c062afeaf30b261d6e821ea8c34",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "69bcfa704ed25a17aeea",
+    "url": "/monsters-rolodex/static/css/main.04de6de0.chunk.css"
+  },
+  {
+    "revision": "c20111b252ca74cb14a6",
+    "url": "/monsters-rolodex/static/js/2.c5359838.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.c5359838.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69bcfa704ed25a17aeea",
+    "url": "/monsters-rolodex/static/js/main.587dd2bc.chunk.js"
+  },
+  {
+    "revision": "e4aea87ee26567f52506",
+    "url": "/monsters-rolodex/static/js/runtime-main.826089ea.js"
+  }
+]);
